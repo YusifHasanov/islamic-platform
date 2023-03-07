eb0fab7eeac618573fe1d594531939439ac9d615
YusifHasanov <yusif.dev.hasanov@gmail.com>
2023-03-07 22:03:45 +0400

    ,askjdbasdbansdkjnsa

--- src/Services/Repositories.ts
+++ src/Services/Repositories.ts
@@ -1,9 +1,9 @@
 // import ArticleRepository from "./concretes/ArticleRepository";
-import BookRepository from "./concretes/BookRepository";
+// import BookRepository from "./concretes/BookRepository";
 import PlaylistRepository from "./concretes/PlaylistRepository";
 import VideoRepository from "./concretes/VideoRepository";
 
 export const playlistRepo = new PlaylistRepository();
 export const videoRepo = new VideoRepository();
 // export const articleRepo = new ArticleRepository();
-export const bookRepo = new BookRepository();
+// export const bookRepo = new BookRepository();
