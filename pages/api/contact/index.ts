2e8b811e90e2d5e84d0c0789ca4cb719d3097e43
YusifHasanov <yusif.dev.hasanov@gmail.com>
2023-03-12 14:42:57 +0400

    comment non-essential files

--- pages/api/contact/index.ts
+++ pages/api/contact/index.ts
@@ -1,31 +1,31 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default function (req: NextApiRequest, res: NextApiResponse) {
-    require('dotenv').config()
+    // require('dotenv').config()
 
-    let nodemailer = require('nodemailer')
-    const transporter = nodemailer.createTransport({
-        port: 465,
-        host: "smtp.gmail.com",
-        auth: {
-            user: 'demo email',
-            pass: process.env.password,
-        },
-        secure: true,
-    })
-    const mailData = {
-        from: 'demo email',
-        to: 'your email',
-        subject: `Message From ${req.body.name}`,
-        text: req.body.message + " | Sent from: " + req.body.email,
-        html: `<div>${req.body.message}</div><p>Sent from:
-      ${req.body.email}</p>`
-    }
-    transporter.sendMail(mailData, function (err: any, info: any) {
-        if (err)
-            console.log(err)
-        else
-            console.log(info)
-    })
+    // let nodemailer = require('nodemailer')
+    // const transporter = nodemailer.createTransport({
+    //     port: 465,
+    //     host: "smtp.gmail.com",
+    //     auth: {
+    //         user: 'demo email',
+    //         pass: process.env.password,
+    //     },
+    //     secure: true,
+    // })
+    // const mailData = {
+    //     from: 'demo email',
+    //     to: 'your email',
+    //     subject: `Message From ${req.body.name}`,
+    //     text: req.body.message + " | Sent from: " + req.body.email,
+    //     html: `<div>${req.body.message}</div><p>Sent from:
+    //   ${req.body.email}</p>`
+    // }
+    // transporter.sendMail(mailData, function (err: any, info: any) {
+    //     if (err)
+    //         console.log(err)
+    //     else
+    //         console.log(info)
+    // })
     res.status(200)
 }
