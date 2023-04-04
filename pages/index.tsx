610a3be7c14462d76b2bbe1a4108d263f85437ae
YusifHasanov <yusif.dev.hasanov@gmail.com>
2023-04-04 18:39:57 +0400

    modified:   prisma/prisma.ts 	modified:   server/routers/_app.router.ts 	renamed:    server/routers/article.router.ts -> server/routers/article.router.txt 	renamed:    server/routers/author.router.ts -> server/routers/author.router.txt 	renamed:    server/routers/book.router.ts -> server/routers/book.router.t 	renamed:    server/routers/youtubeStat.router.ts -> server/routers/youtubeStat.router.txt 	modified:   tsconfig.json

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import Head from 'next/head'
+import React from 'react'
 import { Poppins } from 'next/font/google'
-import { trpc } from '@/server/utils/trpc';
 import Footer from '@/src/components/footer/Footer';
-import Header from '@/src/components/globals/Header';  
+import Header from '@/src/components/globals/Header';
 const montserrat = Poppins({ weight: ["400", "500", "600", "700"], subsets: ["latin-ext"] })
 
 export default function Home(props: any) {
 
-  const p = trpc.playlist.getAll.useQuery(undefined, { staleTime: 86400000, });
-  const v = trpc.video.oneByVideoId.useQuery("uhvn9GtfhLY", { staleTime: 1000 * 60 * 5 });//5min
- 
+
   return (
     <>
       <Header title='Əhli Sünnə Mədrəsəsi' />
       <main className={montserrat.className} >
         <div style={{ height: "80vh" }} className={"text-5xl flex items-center justify-center"} >
           Əhli Sünnə Mədrəsəsi``
-        </div> 
+        </div>
+
       </main>
       <Footer />
     </>
