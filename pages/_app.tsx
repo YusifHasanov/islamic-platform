22fd9eb63f9a88bdb91ad107425fa212e85a1e46
YusifHasanov <yusif.dev.hasanov@gmail.com>
2023-04-04 21:16:48 +0400

    modified:   pages/_app.tsx 	new file:   src/components/globals/MainHeader.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ import { ReactQueryDevtools } from 'react-query/devtools'
 import { SessionProvider } from 'next-auth/react'
 import { trpc } from '@/server/utils/trpc'
 import { useEffect } from 'react'
-import Head from 'next/head'
+import MainHeader from '@/src/components/globals/MainHeader'
 export const queryClient = new QueryClient()
 
 export function reportWebVitals(metric: any) {
@@ -26,42 +26,7 @@ const MyApp: AppType = ({ Component, pageProps }: AppProps) => {
     // <SessionProvider session={pageProps.session}>
 
     <>
-      <Head>
-      <meta name="google-site-verification" content="QR1VwILz70nqB5-xyi6WxnKAWwp155khw37XhUBA-6Q" />
-        <script type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: `
-            {
-              "@context": "https://ehlisunne.vercel.app/",
-              "@type": "WebSite",
-              "mainEntityOfPage": {
-                "@type": "WebPage",
-                "@id": "https://ehlisunne.vercel.app/"
-              },
-              "headline": "Əhli sünnə mədrəsəsi",
-              "description": "Əhli sünnə mədrəsəsi youtube kanalı ilə tanış ola bilərsiniz", 
-              "image": "https://ehlisunne.vercel.app/assets/logo.png",
-              "author": {
-                "@type": "Person",
-                "name": "Yusif Hasanov"
-              },
-              "publisher": {
-                "@type": "Organization",
-                "name": "Əhli sünnə mədrəsəsi",
-                "logo": {
-                  "@type": "ImageObject",
-                  "url": "https://ehlisunne.vercel.app/assets/logo.png"
-                }
-              },
-              "datePublished": "Your publication date here",
-              "dateModified": "Your last modified date here"
-            }
-          `,
-          }}
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta charSet="utf-8" />
-      </Head>
+      <MainHeader />
       <ThemeProvider attribute='class' >
         <QueryClientProvider client={queryClient}>
           <Hydrate state={pageProps.dehydratedState}>
---
+++ src/components/globals/MainHeader.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import Head from 'next/head'
+import Script from 'next/script'
+const MainHeader = () =>
+(
+
+    <Head>
+        <meta name="google-site-verification" content="QR1VwILz70nqB5-xyi6WxnKAWwp155khw37XhUBA-6Q" />
+        <Script
+            src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"
+            strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+            {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){window.dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'GA_MEASUREMENT_ID');
+    `}
+        </Script>
+        <script type="application/ld+json"
+            dangerouslySetInnerHTML={{
+                __html: `
+        
+        {
+          "@context": "https://ehlisunne.vercel.app/",
+          "@type": "WebSite",
+          "mainEntityOfPage": {
+            "@type": "WebPage",
+            "@id": "https://ehlisunne.vercel.app/"
+          },
+          "headline": "Əhli sünnə mədrəsəsi",
+          "description": "Əhli sünnə mədrəsəsi youtube kanalı ilə tanış ola bilərsiniz", 
+          "image": "https://ehlisunne.vercel.app/assets/logo.png",
+          "author": {
+            "@type": "Person",
+            "name": "Yusif Hasanov"
+          },
+          "publisher": {
+            "@type": "Organization",
+            "name": "Əhli sünnə mədrəsəsi",
+            "logo": {
+              "@type": "ImageObject",
+              "url": "https://ehlisunne.vercel.app/assets/logo.png"
+            }
+          },
+          "datePublished": "Your publication date here",
+          "dateModified": "Your last modified date here"
+        }
+      `,
+            }}
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charSet="utf-8" />
+    </Head>
+)
+
+
+export default MainHeader
