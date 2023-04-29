23d79d58e394cd52393831af18433fec5f21d956
Yusif <yusif.hasanov17@gmail.com>
2023-04-29 20:15:07 +0400

    change article title color on homepage

--- src/components/home/Articles.tsx
+++ src/components/home/Articles.tsx
@@ -58,7 +58,7 @@ export default function Articles() {
     <div className="bg-gray-100 dark:bg-gray-900 py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto   lg:mx-0">
-          <h2 className="text-3xl text-center font-bold tracking-tight text:gray-200 dark:text-gray-900 sm:text-4xl">Məqalələrimiz</h2>
+          <h2 className="text-3xl text-center font-bold tracking-tight dark:text-gray-200  text-gray-900 sm:text-4xl">Məqalələrimiz</h2>
           <p className="mt-2 text-lg text-center leading-8 text-gray-600">
             Learn how to grow your business with our expert advice.
           </p>
