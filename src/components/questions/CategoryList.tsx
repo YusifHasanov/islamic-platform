b4ee26260976081e8a89da840356407483e54fc8
Yusif <yusif.hasanov17@gmail.com>
2023-06-27 16:33:42 +0400

    Changes to be committed: 	modified:   src/components/questions/CategoryList.tsx

--- src/components/questions/CategoryList.tsx
+++ src/components/questions/CategoryList.tsx
@@ -3,15 +3,15 @@ import React from 'react'
 
 const CategoryList = () => {
     return (
-        <div className='question_categories  dark:bg-gray-950  flex w-max bg-white items-center mx-4  rounded-3xl  py-3 px-6'>
-            <input type="text" className="bg-gray-50 border   outline-none  border-gray-400 text-gray-900 dark:text-gray-800 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 sticky top-0 block  p-2.5 dark:bg-gray-100 dark:border-gray-600 dark:placeholder-gray-400   dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Axtar" />
+        <div className='question_categories  dark:bg-gray-900  flex w-max bg-white items-center mx-4  rounded-3xl  py-3 px-6'>
+            <input type="text" className="bg-gray-50 border dark:bg-gray-700 dark:border-gray-600   outline-none  border-gray-400 text-gray-900 dark:text-gray-800 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 sticky top-0 block  p-2.5  dark:placeholder-gray-400   dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Axtar" />
             <div className='flex px-2 justify-center items-center overflow-auto categories_container'  >
                 {
                     categories.filter(category => category.parentId === 0).map((parent) => (
                         <div key={parent.id} className="hs-dropdown  relative inline-flex">
-                            <div id="hs-dropdown-with-icons" key={parent.id} className="flex hs-dropdown-toggle items-center justify-between p-2.5 rounded-md hover:bg-blue-100 bg-blue-50 dark:bg-gray-200 mr-1 cursor-pointer">
+                            <div id="hs-dropdown-with-icons" key={parent.id} className="flex  hs-dropdown-toggle items-center justify-between p-2.5 rounded-md hover:bg-blue-100 bg-blue-50 dark:bg-gray-500 hover:dark:bg-gray-600 mr-1 cursor-pointer">
                                 <div className="flex items-center">
-                                    <p className="text-sm text-gray-800      font-semibold ml-2">{parent.name}</p>
+                                    <p className="text-sm text-gray-800  dark:text-gray-300     font-semibold ml-2">{parent.name}</p>
                                 </div> 
                             </div>
                             <div 
@@ -24,7 +24,7 @@ const CategoryList = () => {
                                         .map((child) => (
                                             <a
                                             key={child.id}
-                                                className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-blue-100 mb-1 bg-blue-50 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
+                                                className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-blue-100 mb-1 bg-blue-50 dark:bg-gray-500 dark:text-gray-200 focus:ring-2 focus:ring-blue-500   dark:hover:bg-gray-700 dark:hover:text-gray-300"
                                                 href="#"
                                             >
                                                 {child.name}
