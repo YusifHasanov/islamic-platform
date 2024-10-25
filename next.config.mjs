5f48a7e67b4a7fe78029570a36066e05c45ad399
YusifHasanov <yusif.hasanov17@gmail.com>
2024-10-25 11:31:56 +0400

    ac

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['i.ytimg.com','medrese.vercel.app','hayalhanem.com'],
+    }
+};
+
+export default nextConfig;
