88f6a17d5452006a574ab96fb412694eec66e838
YusifHasanov <yusif.hasanov17@gmail.com>
2024-10-27 14:03:29 +0400

    ac

---
+++ src/app/not-found.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+
+export default function NotFound() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <h1 className="text-3xl font-bold text-red-500 mb-4">404! Not found </h1>
+            <p className="text-gray-700 mb-8">
+                Taplmadi
+            </p>
+
+            <button
+                className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-200"
+            >
+                <Link href={"/"}>
+                    Ana Sayfaya Dön
+                </Link>
+            </button>
+        </div>
+    );
+}
