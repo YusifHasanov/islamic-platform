c8e77fe414d37f8d1e2f18fb7720e50803c365dd
YusifHasanov <yusif.hasanov17@gmail.com>
2024-11-11 18:18:59 +0400

    ac

--- src/components/articles/Pagination.jsx
+++ src/components/articles/Pagination.jsx
@@ -5,16 +5,16 @@ const Pagination = ({ currentPage, totalPages }) => {
     return (
         <div className="pagination flex justify-center items-center gap-4 mt-8">
             {/* Önceki Sayfa */}
-            {currentPage > 1 && (
+            {currentPage > 0 && (
                 <Link href={`/articles?page=${currentPage - 1}`}>
                     <button className="px-4 py-2 bg-gray-200 rounded">Previous</button>
                 </Link>
             )}
 
-            <span>Page {currentPage} of {totalPages}</span>
+            <span>Page {currentPage + 1} of {totalPages }</span>
 
             {/* Sonraki Sayfa */}
-            {currentPage < totalPages && (
+            {currentPage +1 < totalPages && (
                 <Link href={`/articles?page=${currentPage + 1}`}>
                     <button className="px-4 py-2 bg-gray-200 rounded">Next</button>
                 </Link>
