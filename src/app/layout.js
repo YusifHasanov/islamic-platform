e6f09751f3a92e7d6aa8ffa6b09bfdde73bcf2f3
YusifHasanov <yusif.hasanov17@gmail.com>
2024-11-12 21:06:25 +0400

    ac

--- src/app/layout.js
+++ src/app/layout.js
@@ -39,6 +39,7 @@ export default function RootLayout({children}) {
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"/>
+            <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
         </Head>
         <body
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}
