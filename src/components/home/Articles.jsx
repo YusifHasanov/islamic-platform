0897541450d1916a9b724a37fe60ff29f35655ac
yusifhasanov <yusif.hasanov17@gmail.com>
2025-03-25 15:38:28 +0400

    add new keywords.  jj

--- src/components/home/Articles.jsx
+++ src/components/home/Articles.jsx
@@ -11,10 +11,9 @@ const Articles =  async () => {
     console.log("articles", articles);
     return (
         <div className="py-16">
-            <h2 className="text-center homeArticlesHeader  mb-8">MAKALELER</h2>
+            <h2 className="text-center homeArticlesHeader  mb-8">Məqalələr</h2>
             <p className="text-center text-lg mb-12 container mx-auto">
-                Mehmet Yıldız'ın kaleminden, iman hakikatleri, siyer, fıkıh, sahabeler ve birçok önemli konuda derinleşen makaleleriyle sizlere bilgi dolu bir platform sunuyoruz. İslam’ın temel prensiplerini anlamak, tarih boyunca yaşanmış örnekleri keşfetmek ve günlük hayatımıza anlam katmak için yazıldı.
-            </p>
+                İman, Fiqh və Siyarın İşığında – Mənəvi Səyahətinizə Yol Göstərir Bu kanalda, iman həqiqətləri, fiqh və siyer mövzularında hazırlanmış məzmunlarla mənəvi dünyanızı zənginləşdirir, bilgi birikiminizi artırır və doğru səyahətə çıxmanız üçün yol göstərişləri verir. Kanalımızı izləyərək ilim və mənəviyyatın dərinliklərinə çata bilərsiniz.            </p>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-7xl mx-auto px-4">
                 {articles?.map((article, idx) => (
 
@@ -32,7 +31,7 @@ const Articles =  async () => {
             </div>
             <div className="flex justify-center mt-12">
                 <Link href={"/articles"} className="bg-[#F7E652] text-[#fff] px-6 py-2 rounded-2xl font-semibold hover:bg-[#e0d048]">
-                    TÜM MAKALELERİ GÖSTER
+                    Bütün Məqalələri göstər GÖSTER
                 </Link>
             </div>
         </div>
